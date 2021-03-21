87f141105bfc4ef5aa2153972a6feee2a3017e22
Herrfalco <cadet.florian@gmail.com>
2021-03-21 23:23:30 +0400

    normisation

--- src/sorts/backtrack/backtrack.c
+++ src/sorts/backtrack/backtrack.c
@@ -6,7 +6,7 @@
 /*   By: fcadet <fcadet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 17:33:05 by fcadet            #+#    #+#             */
-/*   Updated: 2021/03/21 22:43:40 by fcadet           ###   ########.fr       */
+/*   Updated: 2021/03/21 23:22:59 by fcadet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void		revert_push_exec(t_glob *glob, t_2_stacks *stacks,
 
 	last_op = pop_from_stack(glob->mem, op_stack);
 	glob->op_fn[glob->op_rev[*last_op]](glob, stacks);
-	mem_free_ptr(glob->mem, last_op);	
+	mem_free_ptr(glob->mem, last_op);
 }
 
 static t_bool	useless_move(t_glob *glob, t_2_stacks *stacks,
@@ -33,7 +33,7 @@ static t_bool	useless_move(t_glob *glob, t_2_stacks *stacks,
 	if (op == glob->op_rev[last_op])
 		return (TRUE);
 	if (stacks->a->length < 2 || stacks->b->length < 2)
-		if (op == RRR || op == RR || op == SS) 
+		if (op == RRR || op == RR || op == SS)
 			return (TRUE);
 	if (stacks->a->length < 2)
 		if (op == SA || op == RA || op == RRA)
@@ -46,6 +46,14 @@ static t_bool	useless_move(t_glob *glob, t_2_stacks *stacks,
 	return (FALSE);
 }
 
+static void		change_best_result(t_glob *glob, t_stack **best_result,
+	t_stack *new_result)
+{
+	if (*best_result)
+		stack_free(glob->mem, best_result);
+	*best_result = new_result;
+}
+
 static t_stack	*rec_backtrack_sort(t_glob *glob, t_2_stacks *stacks,
 	t_stack *op_stack)
 {
@@ -56,9 +64,8 @@ static t_stack	*rec_backtrack_sort(t_glob *glob, t_2_stacks *stacks,
 	best_result = NULL;
 	if (op_stack->length > MAX_BACKTRACK)
 		return (NULL);
-	//display_stack(glob, op_stack);
 	if (!stack_entropy(stacks->a, ASCENDING) && !stacks->b->length)
-		return (copy_op_stack(glob, op_stack));	
+		return (copy_op_stack(glob, op_stack));
 	i = -1;
 	while (++i < OP_NUMBER)
 	{
@@ -68,13 +75,9 @@ static t_stack	*rec_backtrack_sort(t_glob *glob, t_2_stacks *stacks,
 		if ((new_result = rec_backtrack_sort(glob, stacks, op_stack)))
 		{
 			if (!best_result || new_result->length < best_result->length)
-			{
-				if (best_result)
-					stack_free(glob->mem, &best_result);
-				best_result = new_result;
-			}
+				change_best_result(glob, &best_result, new_result);
 			else if (new_result)
-				stack_free(glob->mem, &new_result); 
+				stack_free(glob->mem, &new_result);
 		}
 		revert_push_exec(glob, stacks, op_stack);
 	}
--- src/stack/specialized/op_stack.c
+++ src/stack/specialized/op_stack.c
@@ -6,13 +6,13 @@
 /*   By: fcadet <fcadet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 17:33:12 by fcadet            #+#    #+#             */
-/*   Updated: 2021/03/21 21:55:11 by fcadet           ###   ########.fr       */
+/*   Updated: 2021/03/21 23:06:19 by fcadet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/includes.h"
 
-t_op			*new_op(t_glob *glob, t_op op)
+t_op		*new_op(t_glob *glob, t_op op)
 {
 	t_op	*new_op;
 
@@ -38,16 +38,6 @@ void		push_op_to_stack(t_glob *glob, t_stack *stack, char *str)
 		}
 	}
 	error_exit(glob->mem, OP_STR_ERR);
-}
-
-//a supprimer
-void		print_stacks(t_glob *glob, t_2_stacks *stacks)
-{
-	write(1, "A: ", 3);
-	display_stack(glob, stacks->a);
-	write(1, "\nB: ", 4);
-	display_stack(glob, stacks->b);
-	write(1, "\n", 1);
 }
 
 void		exec_op_stack(t_glob *glob, t_stack *op_stack, t_2_stacks *stacks)
